7a5d0cf67ead22f9b8ce0f8f76ca661075731c7b
jjesswan <jessica_wan@brown.edu>
2023-12-05 12:09:20 -0500

    zooming image and depth map

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ from skimage.transform import warp, AffineTransform, EuclideanTransform, Project
 from skimage.color import rgb2gray
 from skimage.measure import ransac
 from skimage.filters import gaussian
+import os
+import argparse
 
-source_file = "data/beatrice.jpg"
-depth_file = ""
+
+source_file = "data/beatrice_full.jpg"
+depth_file = "data/beatrice_full_depth.jpg"
 
 I = cv2.imread(source_file)
 I = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
+D = cv2.imread(depth_file)
+D = cv2.cvtColor(D, cv2.COLOR_BGR2RGB)
+
 u_0 = I.shape[0]/2
 v_0 = I.shape[1]/2
-
-
 f = 35
-# D = cv2.imread(depth_file)
-# D = cv2.cvtColor(D, cv2.COLOR_BGR2RGB)
-
 K = np.array([[f, 0, u_0], [0, f, v_0], [0, 0, 1]])
 
-plt.imshow(I)
-plt.show()
-
-
-# plt.imshow(D)
+# plt.imshow(I)
 # plt.show()
+
+
+# # plt.imshow(D)
+# # plt.show()
 
 # equation 4
 def applyDigitalZoom(input, f, f_desired):
@@ -58,7 +59,46 @@ def applyDigitalZoom(input, f, f_desired):
     # print(np.where(out_im == 0)[3])
     return out_im
 
+def applyDigitalZoomReverse(input, f, f_desired):
+    I = img_as_float32(input[:,:,:])
+    out_im = np.zeros(I.shape)
+    im_width = I.shape[0]
+    im_height = I.shape[1]
 
-output = applyDigitalZoom(I, f, 40)
-plt.imshow(output, cmap="gray")
+    k = f_desired / f
+    u0 = np.array([im_width // 2, im_height // 2])
+    center_offset = (1 - k) * u0
+
+    for o_x in range (im_width):
+        for o_y in range(im_height):
+            [in_x, in_y] = (np.array([o_x, o_y]) - center_offset) / k
+    
+            in_x = int(in_x)
+            in_y = int(in_y)
+
+            if in_x  in range(im_width) and in_y in range(im_height):  
+              out_im[o_x, o_y, :] = I[in_x, in_y, :]
+    # print(np.where(out_im == 0)[3])
+    return out_im
+
+
+output_I = applyDigitalZoomReverse(I, f, 40)
+output_D = applyDigitalZoomReverse(D, f, 40)
+
+f, axarr = plt.subplots(1,2)
+axarr[0].imshow(I)
+axarr[1].imshow(output_I)
 plt.show()
+
+plt.imshow(output_D)
+plt.show()
+
+f2, axarr2 = plt.subplots(1,2)
+axarr2[0].imshow(D)
+axarr2[1].imshow(output_D)
+plt.show()
+
+cv2.imwrite(f'results/beatrice_full_35_to_40.png', output_I)
+cv2.imwrite(f'results/beatrice_full_depth_35_to_40.png', output_D)
+
+
