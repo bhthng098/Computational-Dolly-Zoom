3f20c42a013614c5afb8adedbf282ce375d04f04
beanutbuttr <77995220+bhthng098@users.noreply.github.com>
2023-12-16 18:49:42 -0500

    add args

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
    parser = argparse.ArgumentParser()
    parser.add_argument("-s", help="filepath for source image")
    parser.add_argument("-d", help="filepath for depth image")
-   parser.add_argument("--izoom", help="optional filepath for existing digital zoom image instead of re-generating it")
-   parser.add_argument("--dzoom", help="optional filepath for existing digital zoom depth instead of re-generating it")
-   parser.add_argument("--save", help="flag to save all images. default does not save them", action="store_true")
-   parser.add_argument("--quiet", help="do not show images", action="store_true")
+   parser.add_argument("-fo", help="original focal length in mm")
+   parser.add_argument("-fmin", help="minimum focal length in mm")
+   parser.add_argument("-fd", help="desired focal length in mm")
+   parser.add_argument("--show", help="show intermediate images", action="store_true")
+   parser.add_argument("--use_fusion_fill", help="use the fusion images to fill the holes/gaps", action="store_true")
    args = parser.parse_args()
    
    source_path = args.s
@@ -26,72 +27,48 @@ if __name__ == "__main__":
    depth_map = cv2.imread(depth_path)
    depth_map = cv2.cvtColor(depth_map, cv2.COLOR_BGR2RGB)
 
-   if not args.quiet:
-      show_2_images_side_by_side(orig_image, depth_map, "orig images")
-
-###### HERE ARE THE PARAMETERS WE CAN CHANGE ######
-   f_min = 50
-   f_original = 170 #in pixels, not mm
-   f_desired = 400 #in pixels, not mm
-   step = 40
-           # 1. DIGITAL ZOOM
-   if args.izoom and args.dzoom:
-        i1 = cv2.imread(args.izoom)
-        i1 = cv2.cvtColor(i1, cv2.COLOR_BGR2RGB)
-        d1 = cv2.imread(args.dzoom)
-        d1 = cv2.cvtColor(d1, cv2.COLOR_BGR2RGB)
+   if orig_image is None or depth_map is None:
+     print("invalid filepaths")
    else:
-        i1, d1 = generateDigitallyZoomedImageAndDepth(orig_image, depth_map, f_original, f_desired)
-        if not args.quiet:
-            show_2_images_side_by_side(i1, d1, "digitally zoomed")
-        if args.save:
-            cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-zoomed-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(i1), cv2.COLOR_RGB2BGR))
-            cv2.imwrite(f'results/'+depth_path.split("/", 2)[-1][:-5]+"-zoomed-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(d1), cv2.COLOR_RGB2BGR))
-        
-   t_target = f_min
-   while (f_min <= t_target <= f_desired):
-    t = calculateT(f_original, t_target, depth_map)
-    ###### HERE ARE THE PARAMETERS WE CAN CHANGE ######
-    
-        # 2. DZ SYNTHESIS
-    i_1_dz, d_1_dz = generate_I1_dz_D1_dz(depth_map, i1, d1, t)
-    if not args.quiet:
-        show_2_images_side_by_side(i_1_dz, d_1_dz, "dolly zoom synthesized images 1")
-    if args.save:
-        cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-1dz-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(i_1_dz), cv2.COLOR_RGB2BGR))
-        cv2.imwrite(f'results/'+depth_path.split("/", 2)[-1][:-5]+"-1dz-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(d_1_dz), cv2.COLOR_RGB2BGR))
-    
-    i_2_dz, d_2_dz = generate_I2_dz_D2_dz(depth_map, orig_image, t, f_original, f_desired)
-    if not args.quiet:
-        show_2_images_side_by_side(i_2_dz, d_2_dz, "dolly zoom synthesized images 2")
-    if args.save:
-        cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-2dz-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(i_2_dz), cv2.COLOR_RGB2BGR))
-        cv2.imwrite(f'results/'+depth_path.split("/", 2)[-1][:-5]+"-2dz-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(d_2_dz), cv2.COLOR_RGB2BGR))
-    
-        # IMAGE FUSION OF 1 & 2
-    i_F = image_fusion(i_1_dz, i_2_dz)
-    d_F = image_fusion(d_1_dz, d_2_dz)
+    if args.show:
+        show_2_images_side_by_side(orig_image, depth_map, "orig images")
 
-    if not args.quiet:
-        show_2_images_side_by_side(i_F, d_F, "images fused")
-    if args.save:
-        cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-fused-"+str(f_original)+"-"+str(f_desired)+str(t_target)+'.jpeg', cv2.cvtColor(img_as_ubyte(i_F), cv2.COLOR_RGB2BGR))
-        cv2.imwrite(f'results/'+depth_path.split("/", 2)[-1][:-5]+"-fused-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(d_F), cv2.COLOR_RGB2BGR))
+    f_original = mmToPixels(int(args.fo)) #in pixels, not mm
+    f_desired = mmToPixels(int(args.fd)) #in pixels, not mm
+    
+      # 1. DIGITAL ZOOM
+    
+    i1, d1 = generateDigitallyZoomedImageAndDepth(orig_image, depth_map, f_original, f_desired)
+    if args.show:
+      show_2_images_side_by_side(i1, d1, "digitally zoomed")
+    
+    f_min = mmToPixels(int(args.fmin))
+    t_target = f_min
+    while (f_min <= t_target <= f_desired):
+      t = calculateT(f_original, t_target, depth_map)
+
+      # 2. DZ SYNTHESIS
+      i_1_dz, d_1_dz = generate_I1_dz_D1_dz(depth_map, i1, d1, t)
+
+      i_2_dz, d_2_dz = generate_I2_dz_D2_dz(depth_map, orig_image, t, f_original, f_desired)
+
+        # IMAGE FUSION OF 1 & 2
+      i_F = image_fusion(i_1_dz, i_2_dz)
+      d_F = image_fusion(d_1_dz, d_2_dz)
 
         # HOLE FILLING DEPTH_F
-    hole_filled_D = depth_map_hole_fill(d_F, i_F)
+      hole_filled_D = depth_map_hole_fill(d_F, i_F)
 
-    if not args.quiet:
-        show_2_images_side_by_side(hole_filled_D, d_F, "hole filled depth map")
-    if args.save:
-        cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-fillhole-"+str(f_original)+"-"+str(f_desired)+'.jpeg', cv2.cvtColor(img_as_ubyte(hole_filled_D), cv2.COLOR_RGB2BGR))
+      if args.use_fusion_fill:
+        hole_filled_I = image_hole_filling(hole_filled_D, i_F, i_F)
+        hole_filled_I = depth_map_hole_fill(hole_filled_I, hole_filled_I)
+      else:
+        hole_filled_I = image_hole_filling(hole_filled_D, i_F, i1)
+        hole_filled_I = depth_map_hole_fill(hole_filled_I, hole_filled_I)
 
-
-    hole_filled_I = image_hole_filling(hole_filled_D, i_F, i1)
-    hole_filled_I = depth_map_hole_fill(hole_filled_I, hole_filled_I)
-    if not args.quiet:
-        show_2_images_side_by_side(hole_filled_I, i_F, "hole filled imag3")
-    if args.save:
-        cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-fillholeimage-"+str(f_original)+"-"+str(f_desired)+str(t_target)+'.jpeg', cv2.cvtColor(img_as_ubyte(hole_filled_I), cv2.COLOR_RGB2BGR))
-
-    t_target += step
+      if args.show:
+          plt.imshow(hole_filled_I)
+          plt.show()
+      
+      cv2.imwrite(f'results/'+source_path.split("/", 2)[-1][:-5]+"-fillholeimage-"+str(t_target)+'.jpeg', cv2.cvtColor(img_as_ubyte(hole_filled_I), cv2.COLOR_RGB2BGR))
+      t_target += 10
